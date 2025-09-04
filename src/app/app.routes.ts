b1295251e67e18a0d0a77d216b4b317d4b6a08c7
christianfuerst88 <christian.fuerst@hftm.ch>
2025-09-04 22:23:16 +0200

    refactor(projekt): anpassen und löschen von Dateienan die neuen gegebenheiten

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 // import { BlogComponent } from './blog/blog.component'; // Wird nur bei Eager Loading benötigt
 // import { BlogDetailComponent } from './blog-detail/blog-detail.component'; // Wird nur bei Eager Loading benötigt
-import { blogDetailResolver } from './core/service/blog-detail-resolve/blog-detail-resolve.service';
+// import { blogDetailResolver } from './core/service/blog-detail-resolve/blog-detail-resolve.service';
 
 export const routes: Routes = [
   {
@@ -19,7 +19,7 @@ export const routes: Routes = [
   {
     path: 'blog/:id',
     // component: BlogDetailComponent, // Wird nicht mehr benötig, da dies Eager Loading ist.
-    resolve: { data: blogDetailResolver },
+    // resolve: { data: blogDetailResolver },
     loadComponent: () =>
       import('./features/blog-detail/blog-detail.component').then((c) => c.BlogDetailComponent), // Lazy Loading aktivieren
   },
