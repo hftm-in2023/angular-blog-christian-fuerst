952614bbd79701ffe07390c8fa555053fca2bfed
christianfuerst88 <christian.fuerst@hftm.ch>
2025-09-12 15:23:56 +0200

    feat(router): add-blog lazy route geschütz von isAuthenticatedGuard hinzufügen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-// import { BlogComponent } from './blog/blog.component'; // Wird nur bei Eager Loading benötigt
-// import { BlogDetailComponent } from './blog-detail/blog-detail.component'; // Wird nur bei Eager Loading benötigt
-// import { blogDetailResolver } from './core/service/blog-detail-resolve/blog-detail-resolve.service';
+import { isAuthenticatedGuard } from './core/guards/is-authenticated.guard';
 
 export const routes: Routes = [
   {
@@ -11,18 +8,26 @@ export const routes: Routes = [
     redirectTo: '/blog',
     pathMatch: 'full',
   },
+
   {
     path: 'blog',
-    // component: BlogComponent,
-    loadComponent: () => import('./features/blog/blog.component').then((c) => c.BlogComponent), // Lazy Loading aktivieren
+    loadComponent: () => 
+      import('./features/blog/blog.component').then((c) => c.BlogComponent), // Lazy Loading aktivieren
   },
+
   {
     path: 'blog/:id',
-    // component: BlogDetailComponent, // Wird nicht mehr benötig, da dies Eager Loading ist.
-    // resolve: { data: blogDetailResolver },
     loadComponent: () =>
       import('./features/blog-detail/blog-detail.component').then((c) => c.BlogDetailComponent), // Lazy Loading aktivieren
   },
+
+  {
+    path: 'add-blog',
+    canActivate: [isAuthenticatedGuard], // Top-Level Guard
+    loadChildren: () =>
+      import('./features/add-blog-page/add-blog-page.module').then((m) => m.AddBlogPageModule),
+  },
+
 ];
 
 @NgModule({
