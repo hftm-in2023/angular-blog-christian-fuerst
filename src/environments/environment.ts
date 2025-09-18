ab3078dadd6dd89bbd20e628acb17161faa94a8a
christianfuerst88 <christian.fuerst@hftm.ch>
2025-09-18 21:06:14 +0200

    feat(backend): backend service erstellen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serviceUrl: '',
+  serviceUrl:
+    'https://d-cap-blog-backend---v2.whitepond-b96fee4b.westeurope.azurecontainerapps.io/entries',
 };
