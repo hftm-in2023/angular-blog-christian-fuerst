5b02035d2be8de20fe5739068892197802882241
christianfuerst88 <christian.fuerst@hftm.ch>
2025-09-18 19:18:09 +0200

    fix(forms): template, module und abhängikeiten hinzufügen

--- src/app/features/add-blog/add-blog.component.ts
+++ src/app/features/add-blog/add-blog.component.ts
@@ -1,14 +1,45 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-blog',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    FormsModule, // Grundlegende Formular-Funktionalität
+    ReactiveFormsModule, // Reactive Forms-Direktiven
+    MatFormFieldModule, // Material Form Fields
+    MatInputModule, // Material Input-Komponenten
+    MatButtonModule, // Material Buttons
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <h1>Blog hinzufügen</h1>
-    <p>Das ist eine Dummy Seite.</p>
+
+    <form>
+      <div class="blog-input">
+        <mat-form-field appearance="fill">
+          <mat-label>Title</mat-label>
+          <input matInput />
+          <mat-error></mat-error>
+        </mat-form-field>
+
+        <mat-form-field appearance="fill">
+          <mat-label>Tell your story...</mat-label>
+          <textarea matInput rows="20"></textarea>
+          <mat-error></mat-error>
+        </mat-form-field>
+      </div>
+    </form>
+
+    <button class="submit-button">Publish blog</button>
+
+    <br />
+
     <button [routerLink]="['/blog/']">Zurück</button>
   `,
   styleUrl: './add-blog.component.scss',
